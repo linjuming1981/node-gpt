731b1c8429471ad22f08d8e5a723a1ce49728420
linjuming <mingfish@qq.com>
2024-03-11 00:03:08 +0800

    kkk

---
+++ action.js
@@ -0,0 +1,8 @@
+const Srt = require('./classes/Srt.js')
+
+let Action = {
+  tranSrt(){
+    let srt = new Srt()
+    
+  }
+}
--- classes/Amazon.js
+++ classes/Amazon.js
@@ -8,15 +8,22 @@ class Amazon {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
       }
     });
-    console.log(11, response);
+    // console.log(11, response.data);
     const $ = cheerio.load(response.data);
     
     // 这里你可以进一步解析页面内容，如获取标题,价格等：
-    let title = $('#productTitle').text().trim();
-    let price = $('#priceblock_ourprice').text().trim();
-    
-    console.log(title);
-    console.log(price);
+    let title = $('#title').text().trim();
+    console.log(222, title);
+
+    // featurebullets_feature_div
+    // aplus_feature_div
+    // productDescription_feature_div
+    // productDetails_feature_div
+    let content = $('#bookDescription_feature_div').html()
+    console.log(333, content)
+
+    let img = $('#imgTagWrapperId').html()
+    console.log(444, img)
 
   }
 }
