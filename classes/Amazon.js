838f94a73ac560ec50ca6d7d9488beb64abaf670
linjuming <mingfish@qq.com>
2024-03-13 23:22:07 +0800

    kkk

--- classes/Amazon.js
+++ classes/Amazon.js
@@ -4,33 +4,84 @@ const cheerio = require('cheerio');
 class Amazon {
   async collectPage(url){
     const response = await axios.get(url, {
-      headers: {
-        // 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-        // 'Host': 'www.amazon.com',
-      }
-    })
+      // headers: {
+      //   // 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+      //   // 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+      //   // 'Host': 'www.amazon.com',
+      //   authority : `www.amazon.com`,
+      //   accept: `text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9`,
+      //   'accept-language': `zh-CN,zh;q=0.9,en;q=0.8`,
+      //   // cookie: `session-id=134-9090719-0323652; ubid-main=130-5461362-1770360; s_fid=54073F36B029408D-154A1404B5688AE3; sst-main=Sst1|PQGh-09oT65RGKsPWfl_wcUrB80VY4N66Oe7-KJ64Rw_7t_LDj7_v3UApyR3SEF7_JTaSy3ksjy0GyaaodsnbpyKzdxPq_UG-WZPb4DGBQatNTS6wTABmMnFwNViVgEkqe7bHx_vzFkxkqBsU8V1dPSq0UWx8dBUrlk-26FGgeFSt5w403uNJEuIwrRGvlFYkXPeOV-x-B1eFuoS9pNEGQRytO3LOtlIb7I9Md-ceGNkQAwcXiAFnF4tiW53usBfG_WD; session-id-time=2082787201l; i18n-prefs=USD; lc-main=en_US; session-token=eISlwPsRZWRvKzEiuA9hWC5PS5x3uzU5JWJJsa4Wc/PQCgi7OMU1KlT0+i/3T1i9n/O9fcJabIIZWkETDPvrsq/ZJW9o4M7mAjxJEFv/rWne8YLB4Fxum0OkeFMkXWQluVUr/K3GcKcKvCcpNhPW+ALPbqM1vMmM3Uc+snJcKfMUg0vY1nWaFn5e6AaHQOY4EJYQMydAYB4R97915ljLeXeMW9x8ijKeaM4ppL9RdvrNEGxt7SMf5vDf0lZKAo8m5JqAqaN9Omoy/sdewFr6QNRetb3OUXvfO2O3Clmuo2DnIfPG1HoPjiSZdS41pCXC36YWh4mR0PbXRnqHNFhko7gD+F8I2Uz3; skin=noskin; csm-hit=tb:s-0TRC8GEWX597JDKM3KVW|1710340995550&t:1710340998677&adb:adblk_no`,
+      //   'device-memory':8,
+      //   downlink:1.35,
+      //   dpr: 1,
+      //   ect:'3g',
+      //   rtt: 400,
+      //   'sec-ch-device-memory':8,
+      //   'sec-ch-dpr':1,
+      //   'sec-ch-ua':`"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"`,
+      //   // 'sec-ch-ua-mobile': ?0,
+      //   'sec-ch-ua-platform':"Windows",
+      //   'sec-ch-ua-platform-version':"0.1.0",
+      //   'sec-ch-viewport-width':1073,
+      //   'sec-fetch-dest': 'document',
+      //   'sec-fetch-mode':'navigate',
+      //   'sec-fetch-site':'none',
+      //   // 'sec-fetch-user':?1,
+      //   'upgrade-insecure-requests':1,
+      //   'viewport-width':1073,
+      //   'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+      // },
+      "headers": {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+        "device-memory": "8",
+        "downlink": "1.35",
+        "dpr": "1",
+        "ect": "3g",
+        "rtt": "400",
+        "sec-ch-device-memory": "8",
+        "sec-ch-dpr": "1",
+        "sec-ch-ua": "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-ch-ua-platform-version": "\"0.1.0\"",
+        "sec-ch-viewport-width": "1073",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "viewport-width": "1073",
+        "cookie": "session-id=134-9090719-0323652; ubid-main=130-5461362-1770360; s_fid=54073F36B029408D-154A1404B5688AE3; sst-main=Sst1|PQGh-09oT65RGKsPWfl_wcUrB80VY4N66Oe7-KJ64Rw_7t_LDj7_v3UApyR3SEF7_JTaSy3ksjy0GyaaodsnbpyKzdxPq_UG-WZPb4DGBQatNTS6wTABmMnFwNViVgEkqe7bHx_vzFkxkqBsU8V1dPSq0UWx8dBUrlk-26FGgeFSt5w403uNJEuIwrRGvlFYkXPeOV-x-B1eFuoS9pNEGQRytO3LOtlIb7I9Md-ceGNkQAwcXiAFnF4tiW53usBfG_WD; session-id-time=2082787201l; i18n-prefs=USD; lc-main=en_US; session-token=eISlwPsRZWRvKzEiuA9hWC5PS5x3uzU5JWJJsa4Wc/PQCgi7OMU1KlT0+i/3T1i9n/O9fcJabIIZWkETDPvrsq/ZJW9o4M7mAjxJEFv/rWne8YLB4Fxum0OkeFMkXWQluVUr/K3GcKcKvCcpNhPW+ALPbqM1vMmM3Uc+snJcKfMUg0vY1nWaFn5e6AaHQOY4EJYQMydAYB4R97915ljLeXeMW9x8ijKeaM4ppL9RdvrNEGxt7SMf5vDf0lZKAo8m5JqAqaN9Omoy/sdewFr6QNRetb3OUXvfO2O3Clmuo2DnIfPG1HoPjiSZdS41pCXC36YWh4mR0PbXRnqHNFhko7gD+F8I2Uz3; skin=noskin; csm-hit=tb:s-0TRC8GEWX597JDKM3KVW|1710340995550&t:1710340998677&adb:adblk_no"
+      },
 
-    // console.log(11, response);
-    const $ = cheerio.load(response.data);
-    
-    // 标题
-    let productTitle = $('#productTitle').text().trim();
 
-    // 书籍描述（书籍类型才有）
-    let bookDescription = $('#bookDescription_feature_div').text()
-    
-    // 功能清单
-    let featurebullets = []
-    $('#featurebullets_feature_div li').each((i, n) => {
-      featurebullets.push($(n).text().trim())
     })
+    // .catch(err => console.log(err))
+
+    console.log(11, response);
+    return response.data
+
+    // const $ = cheerio.load(response.data);
     
-    return {
-      productTitle,
-      bookDescription,
-      featurebullets,
-    }
+    // // 标题
+    // let productTitle = $('#productTitle').text().trim();
+
+    // // 书籍描述（书籍类型才有）
+    // let bookDescription = $('#bookDescription_feature_div').text()
+    
+    // // 功能清单
+    // let featurebullets = []
+    // $('#featurebullets_feature_div li').each((i, n) => {
+    //   featurebullets.push($(n).text().trim())
+    // })
+    
+    // return {
+    //   productTitle,
+    //   bookDescription,
+    //   featurebullets,
+    // }
 
   }
 }
