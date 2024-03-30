bf9f466cc197b0024332dea2e3b162acd52685e7
linjuming <mingfish@qq.com>
2024-03-30 16:33:52 +0800

    ddd

--- laji.js
+++ laji.js
@@ -4,10 +4,14 @@
   const GoogleSheet = require('./google-sheet/GoogleSheet.js')
   const gSheet = new GoogleSheet()
   
-  let filter = {markdownCode: '', htmlCode: ''}
   let sheetId = AMAZON_SHEET_ID
   let sheetTabName = '工作表1'
   
-  let datas = await gSheet.getSheetDatas({sheetId, sheetTabName, filter})
-  console.log(datas)
+  // let filter = {markdownCode: '', htmlCode: ''}
+  // let datas = await gSheet.getSheetDatas({sheetId, sheetTabName, filter})
+  // console.log(datas)
+
+  gSheet.updateRow(sheetId, sheetTabName, {productId: '111', productLink: '333', cost:'kkk'})
+
+
 })()
