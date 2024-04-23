36c911082f3eef4ca211c4b78f2192f64f97bccb
linjuming1981 <mingfish@qq.com>
2024-04-23 21:00:50 +0800

    111111

--- public/components/home/index.vue
+++ public/components/home/index.vue
@@ -1,26 +1,41 @@
 <template>
   <div class="home">
     <el-button>test</el-button>
-    <div>aaaaa</div>
+    <el-table :data="products" style="width: 100%">
+      <el-table-column prop="productId" label="productId" width="180" />
+      <el-table-column prop="productTitile" label="productTitile" width="300" >
+        <template #default="scope">
+          <a :href="scope.row.productLink" target="_blank">{{scope.row.productTitile}}</a>
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="Operations" width="120">
+        <template #default="scope">
+          <el-button link type="primary" size="small" @click="handleClick">
+            Detail
+          </el-button>
+          <el-button link type="primary" size="small">Edit</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 <script>
 export default {
   data(){
     return {
-
+      products: []
     }
   },
   methods: {
     async getSheetRows(){
-      let rows = await axios({
+      let res = await axios({
         url: '/getSheetRows',
         method: 'post',
         data: {
           filter: {}
         }
       })
-      console.log(111, rows)
+      this.products = res.data.data
     }
   },
   mounted(){
