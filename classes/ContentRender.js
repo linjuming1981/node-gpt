3c610701c4a7dfaaa95438252930f3216d9b97f7
linjuming1981 <mingfish@qq.com>
2024-04-25 19:55:29 +0800

    11111111

--- classes/ContentRender.js
+++ classes/ContentRender.js
@@ -1,19 +1,24 @@
 const marked = require('marked')
+const fs = require('fs')
 
 class ContentRender {
-  constructor(){}
+  constructor(){
+    this.tplPath = '../public/blogger_tpl.html'
+  }
 
   markdownToHtml(md){
-    let html = marked.parse(md)
+    let mdHtml = marked.parse(md)
+    let html = fs.readFileSync(this.tplPath).toString()
+    html = html.replace('{{mdHtml}}', mdHtml)
     return html
   }
 }
 
 // -------- 调试
-if(module === require.main){
-  let render = new ContentRender()
-  let html = render.markdownToHtml('## title')
-  console.log(html)
-}
+// if(module === require.main){
+//   let render = new ContentRender()
+//   let html = render.markdownToHtml('## title')
+//   console.log(html)
+// }
 
 module.exports = ContentRender
