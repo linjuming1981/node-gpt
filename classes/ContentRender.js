8f724ad7561fc86714e42ae65f9ef56a2129337e
linjuming1981 <mingfish@qq.com>
2024-04-25 20:02:32 +0800

    1111111111

--- classes/ContentRender.js
+++ classes/ContentRender.js
@@ -1,9 +1,10 @@
 const marked = require('marked')
 const fs = require('fs')
+const path = require('path')
 
 class ContentRender {
   constructor(){
-    this.tplPath = '../public/blogger_tpl.html'
+    this.tplPath = path.resolve(__dirname, '../public/blogger_tpl.html');
   }
 
   markdownToHtml(md){
@@ -15,10 +16,10 @@ class ContentRender {
 }
 
 // -------- 调试
-// if(module === require.main){
-//   let render = new ContentRender()
-//   let html = render.markdownToHtml('## title')
-//   console.log(html)
-// }
+if(module === require.main){
+  let render = new ContentRender()
+  let html = render.markdownToHtml('## title')
+  console.log(html)
+}
 
 module.exports = ContentRender
