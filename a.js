4e9f1e3f613c1c3ef06f01ad31d10d19eb93b715
linjuming1981 <mingfish@qq.com>
2024-05-07 21:03:42 +0800

    dddd

--- a.js
+++ a.js
@@ -1,33 +1,35 @@
-const path = require('path');
-const {google} = require('googleapis');
-const {authenticate} = require('@google-cloud/local-auth');
+const { GoogleAdsApi } = require('google-ads-api')
 
-const blogger = google.blogger('v3');
+// 替换以下信息为你的配置
+const client = new GoogleAdsApi({
+    client_id: 'YOUR_CLIENT_ID',
+    client_secret: 'YOUR_CLIENT_SECRET',
+    developer_token: 'YOUR_DEVELOPER_TOKEN',
+})
 
-async function runSample() {
-  // Obtain user credentials to use for the request
-  console.log(111)
-  const auth = await authenticate({
-    // keyfilePath: path.join(__dirname, '../oauth2.keys.json'),
-    keyfilePath: path.join(__dirname, './config/google-oauth2.json'),
-    scopes: 'https://www.googleapis.com/auth/blogger',
-  });
-  console.log(222)
-  google.options({auth});
+const customerId = 'YOUR_CUSTOMER_ID' // Google Ads客户ID
 
-  const res = await blogger.posts.insert({
-    blogId: '8563811807642467410',
-    requestBody: {
-      title: 'Hello from the googleapis npm module!',
-      content:
-        'Visit https://github.com/google/google-api-nodejs-client to learn more!',
-    },
-  });
-  console.log(res.data);
-  return res.data;
+// 错误处理略过，具体请参考API文档
+const main = async () => {
+    const customer = client.Customer({
+        customer_id: customerId,
+        refresh_token: 'YOUR_REFRESH_TOKEN', 
+    })
+
+    const query = `
+        SELECT
+            keyword_plan_forecast_monthly_impressions,
+            keyword_plan_forecast_monthly_clicks,
+            keyword_plan_forecast_average_cpc,
+            keyword_idea_text
+        FROM
+            keyword_plan_idea
+        WHERE
+            keyword_idea_text = 'example keyword'
+    `
+
+    const response = await customer.query(query)
+    console.log(response)
 }
 
-if (module === require.main) {
-  runSample().catch(console.error);
-}
-module.exports = runSample;
+main()
