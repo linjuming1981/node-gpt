bc3639c55d909b115c8d1dbb3becff8744f6cf68
linjuming1981 <mingfish@qq.com>
2024-05-16 20:41:43 +0800

    111111

--- classes/ContentRender.js
+++ classes/ContentRender.js
@@ -7,31 +7,37 @@ class ContentRender {
     this.tplPath = path.resolve(__dirname, '../public/blogger_tpl.html');
   }
 
+  // productToHtml(product){
+  //   let html = fs.readFileSync(this.tplPath).toString()
+
+  //   // markdown生成的html
+  //   let mdHtml = marked.parse(product.markdownCode)
+
+  //   // 视频html
+  //   let arr = JSON.parse(product.videoImgs).map(n => {
+  //     let itHtml = `
+  //       <div class="item">
+  //         <div class="imgBox">
+  //           <img src="${n.imgUrl}" alt="${n.imgDesc}" />
+  //         </div>
+  //         <h4>${n.imgDesc}</h4>
+  //       </div>
+  //     `
+  //     return itHtml
+  //   })
+  //   let videosHtml = arr.join('\n')
+
+  //   // 替换模板
+  //   html = html.replace('{{mdHtml}}', mdHtml)
+  //   html = html.replace('{{videosHtml}}', videosHtml)
+  //   return html
+  // }
+
   productToHtml(product){
-    let html = fs.readFileSync(this.tplPath).toString()
-
-    // markdown生成的html
-    let mdHtml = marked.parse(product.markdownCode)
-
-    // 视频html
-    let arr = JSON.parse(product.videoImgs).map(n => {
-      let itHtml = `
-        <div class="item">
-          <div class="imgBox">
-            <img src="${n.imgUrl}" alt="${n.imgDesc}" />
-          </div>
-          <h4>${n.imgDesc}</h4>
-        </div>
-      `
-      return itHtml
-    })
-    let videosHtml = arr.join('\n')
-
-    // 替换模板
-    html = html.replace('{{mdHtml}}', mdHtml)
-    html = html.replace('{{videosHtml}}', videosHtml)
-    return html
+    let aiResult = JSON.parse(product.aiResult)
+    
   }
+
 }
 
 // -------- 调试
