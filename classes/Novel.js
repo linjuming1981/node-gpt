c96caf0b718baac4cfea55737db40f3f3870bda1
linjuming1981 <mingfish@qq.com>
2024-07-15 15:22:26 +0800

    2222222

--- classes/Novel.js
+++ classes/Novel.js
@@ -16,11 +16,15 @@ class Novel {
   splitBookToChapters(file) {
     let cont = fs.readFileSync(file).toString();
     // 使用正则表达式匹配章节标题和内容
-    let chapters = cont.split(/\n(?=\s*第.*章\s)/);
+    let chapters = cont.split(/\n(?=\s*(第|序).*章\s)/).filter(Boolean); // 分割并过滤掉空白项
     let chapterArr = [];
+    // console.log(chapters)
 
     chapters.forEach(chapter => {
-      let titleMatch = chapter.match(/第.*章\s\S+/);
+      let titleMatch = chapter.match(/(第|序).*章\s\S+/);
+      if(!titleMatch){
+        return true
+      }
       let title = titleMatch ? titleMatch[0] : "Unknown Title";
       let content = chapter.replace(title, '').trim();
       chapterArr.push({ chapterTitle: title, cnCont: content });
@@ -32,7 +36,6 @@ class Novel {
   saveChapterToSheet(items){
     const bookSheet = this.getGSheet();
     bookSheet.addSheetDatas({datas: items})
-    console.log(111)  
   }
 }
 
@@ -41,6 +44,6 @@ const novel = new Novel();
 const file = '../gpt-novels/perfect_world.txt'
 const chapters = novel.splitBookToChapters(file)
 console.log(chapters)
-novel.saveChapterToSheet(chapters)
+// novel.saveChapterToSheet(chapters)
 
 
