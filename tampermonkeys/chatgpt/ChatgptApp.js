ec34f25c30dd061fbebf9fd3e6742317c8312ba9
linjuming1981 <mingfish@qq.com>
2024-07-29 20:06:29 +0800

    333333

---
+++ tampermonkeys/chatgpt/ChatgptApp.js
@@ -0,0 +1,18 @@
+const template = `
+  <div class="chatgpt_app">
+    <button @click="setRole">角色设定</button>
+    <button @click="translate">翻译一章</button>
+  </div>
+`
+
+const ChatgptApp = {
+  template,
+  data(){
+    return {
+
+    }
+  },
+  methods: { 
+   
+  }
+}
---
+++ tampermonkeys/chatgpt_monkey.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         chatgpt
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-21
+// @description  try to take over the world!
+// @author       You
+// @match        https://chagtgpt.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @grant        unsafeWindow
+// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// @require      https://unpkg.com/vue@3.2.26/dist/vue.global.prod.js
+// @require      file://D:\www\leb\node-gpt\classes/AutoTest.js
+// @require      file://D:\www\leb\node-gpt\tampermonkeys\chatgpt\ChatgptApp.js
+// @require      file://D:\www\leb\node-gpt\tampermonkeys\chatgpt_monkey.js
+// ==/UserScript==
+
+(async () => {
+    console.log(111)
+    if (unsafeWindow.Vue === undefined) {
+      unsafeWindow.Vue = Vue;
+    }
+  
+    var el = document.createElement('div');
+    document.body.appendChild(el);
+    const { createApp } = Vue;
+    createApp(ChatgptApp).mount(el)
+  })();
