64769228222f5ecc8abc44d8dacecb86ce609a19
linjuming1981 <mingfish@qq.com>
2024-08-06 20:21:19 +0800

    222222

--- public/components/novel/index.vue
+++ public/components/novel/index.vue
@@ -63,7 +63,7 @@ export default {
         url: '/createNovelBlogPost',
         method: 'post',
         data: {
-          product
+          product: novel
         }
       })
       console.log({res})
