6ece92ca5bc22ab5c10114839fe6de41f9b3d41f
linjuming1981 <mingfish@qq.com>
2024-08-07 18:51:11 +0800

    333

--- server_start.js
+++ server_start.js
@@ -230,7 +230,10 @@ app.post('/createNovelBlogPost', async (req, res) => {
   const blogId = '8875046865650114267' // 小说的博客id
   const ret = await blogger.createPost({blogId, title:product.enTitle, content: html, isDraft: false})
   console.log('/createNovelBlogPost', ret.id);
-  product.postedToBlogger = '1'
+  product = {
+    productId: product.productId,
+    postedToBlogger: '1'
+  }
   novelSheet.updateRow({product})
   res.send({code: 200, ret})
 })
