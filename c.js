cdcd8a3c2c0a3ffa920b5b305fd50c3d9bddf42a
linjuming1981 <mingfish@qq.com>
2024-09-04 09:11:10 +0800

    22222222

--- c.js
+++ c.js
@@ -1,26 +1,49 @@
+const axios = require('axios');
+const OAuth = require('oauth-1.0a');
+const crypto = require('crypto');
 
-// retrieve an access token
-function blogCallback(req, res) {
-  var oauth2Client = getOAuthClient();
-  var code = req.query.code;
-  console.log('code', code)
-  oauth2Client.getToken(code, function (err, tokens) {
-    if (!err) {
-      console.log(1111)
-      oauth2Client.setCredentials(tokens);
-      blogger.posts.insert({
-        auth: oauth2Client,
-        blogId: config.blogId,
-        resource: {
-          title: body['title'],
-          content: body['content']
-        }
-      }, function (xx) {
-        console.log(222, xx)
-        return res.redirect('/');
-      });
-    } else {
-      console.log("error getting blogger API token", err);
-    }
-  });
+// 1. 设置你的 API 密钥和访问令牌
+const apiKey = 'A6ObcfxZVoetRs19p2RucDFb6';
+const apiSecretKey = 'CbgTPyIvYPWF8iUdIiLkwNwHzK2CkZMKQ7X5ecZE95kXr07ZpB';
+const accessToken = '420767326-FqeiNTaogrv8emHieBqICtAMJ6joOCIrg1SYLxLD';
+const accessTokenSecret = 'lPw412aWdWmq4gdxxOtlSdNwek9uFTSE2Itr4GXHfu70E';
+
+// 2. 创建 OAuth 1.0a 实例
+const oauth = OAuth({
+  consumer: { key: apiKey, secret: apiSecretKey },
+  signature_method: 'HMAC-SHA1',
+  hash_function(baseString, key) {
+    return crypto.createHmac('sha1', key).update(baseString).digest('base64');
+  },
+});
+
+const token = {
+  key: accessToken,
+  secret: accessTokenSecret,
+};
+
+// 3. 发帖到 X.com
+async function postToX(content) {
+  const url = 'https://api.twitter.com/2/tweets';
+  const requestData = {
+    url,
+    method: 'POST',
+    data: { text: content },
+  };
+
+  try {
+    const response = await axios.post(url, requestData.data, {
+      headers: {
+        Authorization: oauth.toHeader(oauth.authorize(requestData, token)).Authorization,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    console.log('Tweet posted successfully:', response.data);
+  } catch (error) {
+    console.error('Error posting tweet:', error.response ? error.response.data : error.message);
+  }
 }
+
+// 4. 调用函数发帖
+postToX('Hello, World! This is a post from my Node.js app.');
