31ac47293b80ffae8c334ddb8791e7048f540fe9
linjuming1981 <mingfish@qq.com>
2024-09-11 11:32:33 +0800

    1111111

--- a.js
+++ a.js
@@ -15,32 +15,32 @@ async function createPost() {
   try {
     await client.createPost(blogName, {
       content: [
-        {
-          type: 'image',
-          media: fs.createReadStream(path.resolve(__dirname, './output.png')),  
-          // url: 'https://i.imgur.com/j9B0am3.jpeg',    
-          alt_text: '…',
-          attribution: {
-            "type": "link",
-            "url": "http://shahkashani.com/"
-          }
-        },
-        {
-          type: 'text',
-          text: 'This is a sample post with an <a href="https://example.com">example link</a>'
-        },
+        // {
+        //   type: 'image',
+        //   media: fs.createReadStream(path.resolve(__dirname, './output.png')),  
+        //   // url: 'https://i.imgur.com/j9B0am3.jpeg',    
+        //   alt_text: '…',
+        //   attribution: {
+        //     "type": "link",
+        //     "url": "http://shahkashani.com/"
+        //   }
+        // },
+        // {
+        //   type: 'text',
+        //   text: 'This is a sample post with an <a href="https://example.com">example link</a>'
+        // },
         {
           "type": "link",
           "url": "https://www.nytimes.com/2017/06/15/us/politics/secrecy-surrounding-senate-health-bill-raises-alarms-in-both-parties.html",
-          "title": "Secrecy Surrounding Senate Health Bill Raises Alarms in Both Parties",
+          // "title": "Secrecy Surrounding Senate Health Bill Raises Alarms in Both Parties",
           "description": "Senate leaders are writing legislation to repeal and replace the Affordable Care Act without a single hearing on the bill and without an open drafting session.",
           "author": "Thomas Kaplan and Robert Pear",
           "poster": [
               {
-                  "url": "https://static01.nyt.com/images/2017/06/15/us/politics/15dchealth-2/15dchealth-2-facebookJumbo.jpg",
+                  "url": "https://i.imgur.com/j9B0am3.jpeg",
                   "type": "image/jpeg",
-                  "width": 1050,
-                  "height": 549
+                  "width": 1024,
+                  "height": 1024
               }
           ]
        }
