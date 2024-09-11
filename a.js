054e67462e73f2829923fe6a1b2da83be28938f9
linjuming1981 <mingfish@qq.com>
2024-09-11 09:47:13 +0800

    ddddddd

--- a.js
+++ a.js
@@ -1,35 +1,31 @@
-const axios = require('axios');
-const fs = require('fs')
+const tumblr = require('tumblr.js');
+const fs = require('fs');
+const path = require('path');
 
-const blogName = 'linjuming.tumblr.com';
-const apiUrl = `https://api.tumblr.com/v2/blog/${blogName}/posts`;
+const client = tumblr.createClient({
+  consumer_key: 'gzBMbQWEVfOcY2zClcVzpzCP8QP2XcrDpmGAYQWLNuDCFBfVik',
+  consumer_secret: 'ZI9p4Yxpaee3EqE0bbQtx7YSIR5RBEpw3RR8I4m4XZiuMjv5cI',
+  token: 'Gpr1j56qVORXdp7HZvWYICTqEVf1WsarXbJaLLRvfG85EaRkRM',
+  token_secret: 'YoiZ4cFMdGxCXFlGj5b5vUdI6pLMR8fSjOEn0WdG0e6e2O7aSB'
+});
 
-// 读取图片文件
-const imagePath = './temp/output.png';
-const imageData = fs.readFileSync(imagePath);
-// console.log(imageData) // 确定有数据    
+const blogName = 'linjuming';  // 替换为你的博客名称
 
-const postData = {
-  type: 'photo',
-  caption: 'This is a photo post',
-  tags: ['nodejs', 'tumblr'],
-  data: [imageData]
-};
-
-const config = {
-  params: {
-    api_key: 'gzBMbQWEVfOcY2zClcVzpzCP8QP2XcrDpmGAYQWLNuDCFBfVik'
-  },
-  headers: {
-    'Content-Type': 'application/json'
-  }
-};
-
-axios.post(apiUrl, postData, config)
-  .then(response => {
-    console.log(11111111)    
-    console.log('Post created:', response.data.response.id);
-  })
-  .catch(error => {
+async function createPost() {
+  try {
+    await client.createPost(blogName, {
+      content: [
+        {
+          type: 'image',
+          media: fs.createReadStream(path.resolve(__dirname, './temp/output.png')),
+          alt_text: '…',
+        },
+      ],
+    });
+    console.log('Post created successfully');
+  } catch (error) {
     console.error('Error creating post:', error);
-  });
+  }
+}
+
+createPost();
