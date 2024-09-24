446f7157cf5af1aa60ff805abc9d780ba1a66235
linjuming1981 <mingfish@qq.com>
2024-09-24 20:51:28 +0800

    1111111

--- a.js
+++ a.js
@@ -1,65 +1,19 @@
-const tumblr = require('tumblr.js');
-const fs = require('fs');
-const path = require('path');
-
-const client = tumblr.createClient({
-  consumer_key: 'gzBMbQWEVfOcY2zClcVzpzCP8QP2XcrDpmGAYQWLNuDCFBfVik',
-  consumer_secret: 'ZI9p4Yxpaee3EqE0bbQtx7YSIR5RBEpw3RR8I4m4XZiuMjv5cI',
-  token: 'Gpr1j56qVORXdp7HZvWYICTqEVf1WsarXbJaLLRvfG85EaRkRM',
-  token_secret: 'YoiZ4cFMdGxCXFlGj5b5vUdI6pLMR8fSjOEn0WdG0e6e2O7aSB'
-});
-
-const blogName = 'linjuming';  // 替换为你的博客名称
-
-async function createPost() {
-  try {
-    const res = await client.createPost(blogName, {
-      content: [
-        // {
-        //   type: 'image',
-        //   // media: fs.createReadStream(path.resolve(__dirname, './output.png')),  
-        //   // url: 'https://i.imgur.com/j9B0am3.jpeg',  
-        //   "url": "http://69.media.tumblr.com/b06fe71cc4ab47e93749df060ff54a90/tumblr_nshp8oVOnV1rg0s9xo1_1280.jpg",  
-        //   "alt_text": "Sonic the Hedgehog and friends",
-        //   "caption": "I'm living my best life on earth.",
-        // },
-        {
-          "type": "image",
-          "caption": "I'm living my best life on earth.",
-          "media": [
-              {
-                  "type": "image/jpeg",
-                  "url": "https://i.imgur.com/j9B0am3.jpeg",
-                  "width": 1024,
-                  "height": 1024
-              }
-          ]
-        },
-        {
-          type: 'text',
-          text: 'Senate leaders are writing legislation to Secrecy Surrounding '.repeat(20)
-        },
-        {
-          "type": "link",
-          "url": "https://novel-focus.blogspot.com/2024/09/chapter-20-sudden-change.html",
-          // "title": "Secrecy Surrounding Senate Health Bill Raises Alarms in Both Parties",
-          "description": "",
-          // "author": "Thomas Kaplan and Robert Pear",
-          // "poster": [
-          //     {
-          //         "url": "https://i.imgur.com/j9B0am3.jpeg",
-          //         "type": "image/jpeg",
-          //         "width": 1024,
-          //         "height": 1024
-          //     }
-          // ]
-       }
-      ],
-    });
-    console.log('Post created successfully', res);
-  } catch (error) {
-    console.error('Error creating post:', error);
+async function query(data) {
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/suno/bark",
+    {
+      headers: {
+        Authorization: "Bearer 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",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    }
+  );
+    const result = await response.blob();
+    return result;
   }
-}
-
-createPost();
+  query({"inputs": "The answer to the universe is 42"}).then((response) => {
+    // Returns a byte object of the Audio wavform. Use it directly!
+    console.log(response)
+  });
