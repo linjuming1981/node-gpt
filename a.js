9ac3d7f7942765d3d78c6ff4ca48059f375c1fad
linjuming1981 <mingfish@qq.com>
2024-09-27 20:28:25 +0800

    11111

--- a.js
+++ a.js
@@ -1,28 +1,35 @@
-const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
+const fetch = require('node-fetch');
 
 async function query(data) {
   try {
-    const response = await axios.post(
-      "https://api-inference.huggingface.co/models/microsoft/speecht5_tts",
-      data,
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/suno/bark",
       {
+        method: "POST",
         headers: {
           Authorization: "Bearer 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",
           "Content-Type": "application/json",
         },
-        responseType: 'arraybuffer' // 处理二进制数据
+        body: JSON.stringify(data),
       }
     );
-    
+
+    if (!response.ok) {
+      // throw new Error(`HTTP error! status: ${response.status}`);
+      console.log(response)
+    }
+
+    const audioBuffer = await response.buffer(); // 获取音频缓冲数据
+
     // 将音频文件保存到本地
     const filePath = path.join(__dirname, 'output.wav');
-    fs.writeFileSync(filePath, response.data);
+    fs.writeFileSync(filePath, audioBuffer);
     console.log(`音频已保存至 ${filePath}`);
   } catch (error) {
     console.error("错误:", error);
   }
 }
 
-query({"inputs": "The answer to the universe is 42"});
+query({"inputs": "hello world"});
