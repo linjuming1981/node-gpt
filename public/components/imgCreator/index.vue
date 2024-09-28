66b9efc751fea6b088313c6b501a03cfd72041c5
linjuming1981 <mingfish@qq.com>
2024-09-28 10:47:38 +0800

    11111

--- public/components/imgCreator/index.vue
+++ public/components/imgCreator/index.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="prompt_line">
       <span>prompt:</span>
-      <el-input type="textarea" v-model="imgPrompt"></el-input>
+      <el-input class="imgPrompt" type="textarea" v-model="imgPrompt"></el-input>
       <el-button type="primary" @click="createImg">生成</el-button>
     </div>
   </div>
@@ -40,5 +40,16 @@ export default {
 .preview_line{}
 .prompt_line{
   display: flex;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+  align-items: flex-end;
+  background-color: #edf0f3;
+  border-top: 1px solid #bdbdbd;
+}
+.imgPrompt{
+  margin: 0 20px 0 20px;
 }
 </style>
