f3ba87cc328f74c4b2df55b0dfb956a1b6acc3aa
linjuming <mingfish@qq.com>
2024-10-13 01:02:43 +0800

    'update'

--- routes/workers.js
+++ routes/workers.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/worker/yahoo-nba-to-twitter', async(req, res) => {
-  // let rssUrl = 'https://sports.yahoo.com/nba/rss/';
-  // const Yahoo = require('../classes/Yahoo.js')
-  // const yahoo = new Yahoo({ rssUrl, sheetName: 'yahoo_nba' });
-  // const article = await yahoo.postNewArticleToTwitter()
-  const article = {'kkk':'jjj'}
-  console.log(article)
+  let rssUrl = 'https://sports.yahoo.com/nba/rss/';
+  const Yahoo = require('../classes/Yahoo.js')
+  const yahoo = new Yahoo({ rssUrl, sheetName: 'yahoo_nba' });
+  const article = await yahoo.postNewArticleToTwitter()
+  console.log('/worker/yahoo-nba-to-twitter', article)
   res.send({code: 200, article})
 })
 
