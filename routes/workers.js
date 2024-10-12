3fab9201d311045fa71b9a5c04f8a2a629d181de
linjuming <mingfish@qq.com>
2024-10-12 16:36:52 +0000

    push in google shell

--- routes/workers.js
+++ routes/workers.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/worker/yahoo-nba-to-twitter', async(req, res) => {
-  const article = {title: 'text'}
+  // let rssUrl = 'https://sports.yahoo.com/nba/rss/';
+  // const Yahoo = require('../classes/Yahoo.js')
+  // const yahoo = new Yahoo({ rssUrl, sheetName: 'yahoo_nba' });
+  // const article = await yahoo.postNewArticleToTwitter()
+  const article = {'kkk':'jjj'}
   res.send({code: 200, article})
 })
 
