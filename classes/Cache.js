d651c95305f0fb6ab0fb466f02f0660d41ea6c9c
linjuming1981 <mingfish@qq.com>
2024-10-16 21:09:49 +0800

    22222

--- classes/Cache.js
+++ classes/Cache.js
@@ -2,8 +2,8 @@ const LRU = require('lru-cache');
 
 // 配置缓存大小和过期时间
 const cache = new LRU({
-  max: 100,              // 最大缓存数
-  ttl: 60 * 60 * 1000,   // 1小时的过期时间（以毫秒为单位）
+  max: 100,             // 最大缓存条目数
+  ttl: 60 * 60 * 1000   // 1小时的过期时间（以毫秒为单位）
 });
 
 const Cache = {
@@ -16,7 +16,7 @@ const Cache = {
   },
 
   delete(key) {
-    cache.delete(key); // v11 使用 delete 方法
+    cache.delete(key); // 删除缓存项，LRU v7+ 使用 delete
   }
 };
 
